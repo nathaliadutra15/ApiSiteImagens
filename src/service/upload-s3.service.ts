cb43021c7f810614e480efefbea3157272f80d7e
Nathalia Dutra <nathaliad.oliveira@hotmail.com>
2023-10-18 08:25:07 -0300

    Add arquivo service do upload

---
+++ src/service/upload-s3.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+
+
+@Injectable()
+export class UploadS3Service {
+    constructor(){}
+
+    uploadOnS3(file: Express.Multer.File) {
+        
+    }
+
+} 
