79311ba47c9f8ea2e4882afe18661641adf8f817
Nathalia Dutra <nathaliad.oliveira@hotmail.com>
2023-11-02 15:25:51 -0300

    Alterado retorno do endpoint de criacao de comentário - v2

--- src/controller/comment.controller.ts
+++ src/controller/comment.controller.ts
@@ -13,8 +13,7 @@ export class CommentController {
             comment.criadoEm = new Date();
             comment.atualizadoEm = new Date();
             await this.commentService.createComment(_postId._postId, comment);
-            let comments = await this.commentService.getCommentsById(_postId._postId);
-            return res.status(201).send(comments["posts"].map(item => item.comentarios).flat());
+            return res.status(201).send(comment);
         } catch (error) {
             return res.status(500).send({ message: error });
         }
